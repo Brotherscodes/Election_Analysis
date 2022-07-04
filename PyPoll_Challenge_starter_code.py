d7ae8fc2b52006fa4c884ab0f0951fbaf558bbca
Dylan Brothers <dylanbrothers@hotmail.com>
2022-07-03 22:35:33 -0400

    Adjusting \n in Final commit of election_analysis.txt file.

--- PyPoll_Challenge_starter_code.py
+++ PyPoll_Challenge_starter_code.py
@@ -93,7 +93,7 @@ with open(file_to_save, "w") as txt_file:
         f"-------------------------\n"
         f"Total Votes: {total_votes:,}\n"
         f"-------------------------\n\n"
-        f"County Votes:\n")
+        f"County Votes:\n\n")
     print(election_results, end="")
 
     txt_file.write(election_results)
@@ -152,7 +152,7 @@ with open(file_to_save, "w") as txt_file:
         votes = candidate_votes.get(candidate_name)
         vote_percentage = float(votes) / float(total_votes) * 100
         candidate_results = (
-            f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+            f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n\n")
 
         # Print each candidate's voter count and percentage to the
         # terminal.
